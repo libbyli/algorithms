e79fe95cca882c144d2335b7f1d22ef42747bdfe
Elizabeth Li <sincerelylibby@gmail.com>
2018-07-29 19:42:15 -0700

    Implement alt solution without data structure.

--- isUnique.js
+++ isUnique.js
@@ -1,5 +1,4 @@
 // Implement an algorithm to determine if a string has all unique characters.
-
 const isUnique = (string) => {
   const characters = {};
   for (let i = 0; i < string.length; i += 1) {
@@ -7,6 +6,24 @@ const isUnique = (string) => {
       characters[string[i]] = true;
     } else {
       return false;
+    }
+  }
+  return true;
+};
+
+isUnique('hello'); // false
+isUnique('abcd'); // true
+isUnique('h'); // true
+isUnique(''); // true
+
+
+// What if you cannot use additional data structures?
+const isUnique2 = (string) => {
+  for (let i = 0; i < string.length; i += 1) {
+    for (let j = i + 1; j < string.length; j += 1) {
+      if (string[i] === string[j]) {
+        return false;
+      }
     }
   }
   return true;
