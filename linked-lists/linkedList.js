2309c1d3567b59f1ec0f79aa8850077fff21f835
Elizabeth Li <sincerelylibby@gmail.com>
2018-08-03 16:45:57 -0700

    Implement LinkedList.

---
+++ linked-lists/linkedList.js
@@ -0,0 +1,51 @@
+const Node = (value) => {
+  const node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+};
+
+const LinkedList = () => {
+  const list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = (value) => {
+    const newTail = Node(value);
+
+    if (!list.head) {
+      list.head = newTail;
+    }
+
+    if (list.tail) {
+      list.tail.next = newTail;
+    }
+
+    list.tail = newTail;
+  };
+
+  list.removeHead = () => {
+    if (list.head === null) {
+      return null;
+    }
+
+    const currentHead = list.head;
+    list.head = list.head.next;
+
+    return currentHead.value;
+  };
+
+  list.contains = (target) => {
+    let node = list.head;
+
+    while (node) {
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
+  };
+};
