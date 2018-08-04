20404efc5c0307b2e92ee7f28759ec6e59f16e11
Elizabeth Li <sincerelylibby@gmail.com>
2018-08-03 17:01:20 -0700

    Implement stringRotation.

---
+++ arrays-and-strings/stringRotation.js
@@ -0,0 +1,13 @@
+/* Assume you have a method isSubstring which checks if one word is a substring of another.
+Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call
+to isSubstring (e.g., "waterbottle" is a rotation of "erbottlewat"). */
+
+const isStringRotation = (s1, s2) => {
+  if (s1.length !== s2.length || s1.length === 0) {
+    return false;
+  }
+
+  const s1Double = s1 + s1;
+  return isSubstring(s1Double, s2);
+};
+
