07b9389812996b61cf9d0584d285119d29a0c2d2
Elizabeth Li <sincerelylibby@gmail.com>
2018-08-26 18:56:28 -0700

    Work on minimalTree.

--- trees-and-graphs/minimalTree.js
+++ trees-and-graphs/minimalTree.js
@@ -1,6 +1,27 @@
 /* Given a sorted (increasing order) array with unique integer elements, write an algorithm
 to create a binary search tree with minimal height. */
 
-const createMinimalTree = (array) => {
-  
+const createMinimalTree = (arr) => {
+  if (arr.length === 0) {
+    return null;
+  }
+  const result = [];
+
+  let createTree = (arr) => {
+    if (arr.length === 0) {
+      return;
+    }
+    let midpoint = Math.floor(arr.length / 2);
+    result.push(arr[midpoint]);
+    const leftArr = arr.slice(0, midpoint);
+    const rightArr = arr.slice(midpoint + 1, arr.length);
+
+    createTree(leftArr);
+    createTree(rightArr);
+  };
+
+  createTree(arr);
+  return result;
 };
+
+createMinimalTree([1, 2, 3, 4, 5, 6, 7]); // [4, 2, 6, 1, 3, 5, 7];
